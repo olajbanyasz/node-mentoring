0422ced791f2a02ef7b3aee7903496a0b3f776ef
Zsolt Andre <zsolt_andre@epam.com>
2023-05-08 13:06:54 +0200

    parse with events added

--- src/hw1/task2.ts
+++ src/hw1/task2.ts
@@ -8,7 +8,7 @@ const TEXT_FILE_PATH = path.join(__dirname, './output.txt')
 const writeStream = createWriteStream(TEXT_FILE_PATH);
 const readStream = createReadStream(CSV_FILE_PATH);
 const converter = csv({ delimiter: [';', ',', '|'] });
-
+const { EventEmitter } = require('events');
 
 const parseCsvWithPipeline = () => {
   pipeline(
@@ -35,4 +35,33 @@ const parseCsvWithPipe = () => {
   });
 }
 
-parseCsvWithPipeline();
+const parseCsvWithEvents = () => {
+  const eventEmitter = new EventEmitter();
+
+  csv()
+    .fromFile(CSV_FILE_PATH)
+    .on('data', (data: Buffer) => {
+      eventEmitter.emit('data', data);
+    })
+    .on('done', () => {
+      eventEmitter.emit('done');
+    })
+    .on('error', (err : Error) => {
+      eventEmitter.emit('error', err);
+    });
+
+  eventEmitter.on('data', (data: string) => {
+    writeStream.write(data + '\n');
+  });
+
+  eventEmitter.on('error', (err: Error) => {
+    console.error('Failed to write data txt file', err);
+  });
+
+  eventEmitter.on('done', () => {
+    writeStream.end();
+    console.log('Data successfully wrote to txt file.');
+  });
+};
+
+parseCsvWithEvents();

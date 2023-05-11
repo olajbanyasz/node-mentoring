935d8c6b89a832bbfb33883244fa5cf3008dae1a
André Zsolt <andrezsolt71@gmail.com>
2023-05-11 09:39:18 +0200

    Homework week 1 (#1)
    * Homework week 1
    
    * parse with events added
    
    * import and parsing fix
    
    * review fixes
    
    ---------
    
    Co-authored-by: Zsolt Andre <zsolt_andre@epam.com>

---
+++ src/hw1/task2.ts
@@ -0,0 +1,67 @@
+import { createReadStream, createWriteStream, appendFileSync } from 'fs';
+import path from 'path';
+import csv from 'csvtojson';
+import { pipeline } from 'stream';
+import { EventEmitter } from 'events';
+
+const CSV_FILE_PATH = path.join(__dirname, './input.csv');
+const TEXT_FILE_PATH = path.join(__dirname, './output.txt')
+const writeStream = createWriteStream(TEXT_FILE_PATH);
+const readStream = createReadStream(CSV_FILE_PATH);
+const converter = csv({ delimiter: [';', ',', '|'] });
+
+const parseCsvWithPipeline = () => {
+  pipeline(
+    readStream,
+    converter, 
+    writeStream,
+    (err) => {
+      if (err) {
+        console.error('Failed to write data txt file', err);
+      } else {
+        console.log('Data successfully wrote to txt file.');
+      }
+    }
+  );
+};
+
+const parseCsvWithPipe = () => {
+  readStream.pipe(converter).pipe(writeStream);
+  writeStream.on('finish', () => {
+    console.log('Data successfully wrote to txt file.');
+  })
+  .on('error', (err: Error) => {
+    console.error('Failed to write data txt file', err);
+  });
+}
+
+const parseCsvWithEvents = () => {
+  const eventEmitter = new EventEmitter();
+
+  csv()
+    .fromFile(CSV_FILE_PATH)
+    .on('data', (data: Buffer) => {
+      eventEmitter.emit('data', data);
+    })
+    .on('done', () => {
+      eventEmitter.emit('done');
+    })
+    .on('error', (err : Error) => {
+      eventEmitter.emit('error', err);
+    });
+
+  eventEmitter.on('data', (data: string) => {
+    writeStream.write(data);
+  });
+
+  eventEmitter.on('error', (err: Error) => {
+    console.error('Failed to write data txt file', err);
+  });
+
+  eventEmitter.on('done', () => {
+    writeStream.end();
+    console.log('Data successfully wrote to txt file.');
+  });
+};
+
+parseCsvWithEvents();

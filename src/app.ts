e961ad68f3de2373e163042733527b875a3e8cb0
Zsolt Andre <zsolt_andre@epam.com>
2023-06-23 15:26:13 +0200

    logger + error handling added

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 import bodyParser from 'body-parser';
-import express, { Express} from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import routers from './routers';
 import { sq } from './data-access/db';
+import logger from './middlewares/logger';
+import errorHandler from './middlewares/errorHandler';
+import morganMiddleware from './middlewares/morganMiddleware';
 
 const app: Express = express();
 dotenv.config();
 const PORT: string | number = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(morganMiddleware);
 app.use(...routers);
-
-sq.sync();
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
+  sq.sync();
+
+  logger.info(`Server is listening on http://localhost:${PORT}`);
 });
+
+process
+  .on('unhandledRejection', (reason, promise) => {
+    logger.error(`UnhandledRejection at ${promise}, reason: ${reason}`);
+  })
+  .on('uncaughtException', (error, origin) => {
+    logger.error(`UncaughtException: ${error}, origin: ${origin}`);
+  });
--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -1,43 +1,60 @@
 import GroupService from '../services/group.service';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { Group, Permission } from '../utils/shapes';
 import { StatusCodes } from 'http-status-codes';
 
 const groupService = new GroupService();
 
-export async function getGroups(req: Request, res: Response) {
+export async function getGroups(request: Request, response: Response, next: NextFunction) {
+  try { 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const groups : Group[] | any[] = await groupService.getAllGroup();
-    res.status(StatusCodes.OK).json(groups);
-}
-
-export async function addGroup(req: Request, res: Response): Promise<void> {
-  const name: string = req.body.name;
-  const permissions: Array<Permission> = req.body.permissions;
-  await groupService.createGroup(name, permissions);
-  res.status(StatusCodes.OK).json('New group is created');
-}
-
-export async function deleteGroup(req: Request, res: Response): Promise<void> {
-  const id = Number(req.params.id);
-  await groupService.deleteGroup(id);
-  res.status(StatusCodes.OK).json('Group is deleted');
-}
-
-export async function getGroup(req: Request, res: Response): Promise<void> {
-  const id = Number(req.params.id);
-  try {
-    const group = await groupService.getGroup(id);
-    res.status(StatusCodes.OK).json(group);
+    response.status(StatusCodes.OK).json(groups);
   } catch (error) {
-    res.status(StatusCodes.NOT_FOUND).json('Group does not exist');
+    return next(error);
   }
 }
 
-export async function updateGroup(req: Request, res: Response): Promise<void> {
-  const id = Number(req.params.id);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const group: Group | any = await groupService.getGroup(id);
-  const updatedGroup = Object.assign({}, group, req.body);
-  await groupService.updateGroup(updatedGroup);
-  res.status(StatusCodes.OK).json('Update success');
+export async function addGroup(request: Request, response: Response, next: NextFunction) {
+  try {
+    const name: string = request.body.name;
+    const permissions: Array<Permission> = request.body.permissions;
+    await groupService.createGroup(name, permissions);
+    response.status(StatusCodes.OK).json('New group is created');
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function deleteGroup(request: Request, response: Response, next: NextFunction) {
+  try {
+    const id = Number(request.params.id);
+    await groupService.deleteGroup(id);
+    response.status(StatusCodes.OK).json('Group is deleted');
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function getGroup(request: Request, response: Response, next: NextFunction) {
+  try {
+    const id = Number(request.params.id);
+    const group = await groupService.getGroup(id);
+    response.status(StatusCodes.OK).json(group);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function updateGroup(request: Request, response: Response, next: NextFunction) {
+  try {
+    const id = Number(request.params.id);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const group: Group | any = await groupService.getGroup(id);
+    const updatedGroup = Object.assign({}, group, request.body);
+    await groupService.updateGroup(updatedGroup);
+    response.status(StatusCodes.OK).json('Update success');
+  } catch (error) {
+    return next(error);
+  }
 }
